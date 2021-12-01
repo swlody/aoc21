bcb9da94183f20a2fc2cee734f169cf0a155208c
Sam Wlody <swlody@gmail.com>
2021-12-01 10:10:07 -0500

    Better(?) day 1 solutions using Itertools::tuple_windows()

--- src/day01.rs
+++ src/day01.rs
@@ -1,34 +1,32 @@
+use itertools::Itertools;
+
 pub fn input_generator(input: &str) -> Vec<u32> {
     input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-pub fn solve_part1(depths: &[u32]) -> u32 {
-    let mut num_increases = 0;
-    for i in 1..depths.len() {
-        if depths[i] > depths[i - 1] {
-            num_increases += 1;
-        }
-    }
-    num_increases
+pub fn solve_part1(depths: &[u32]) -> usize {
+    depths
+        .iter()
+        .tuple_windows()
+        .filter(|(last_depth, current_depth)| current_depth > last_depth)
+        .count()
 }
 
-pub fn solve_part2(depths: &[u32]) -> u32 {
-    let mut num_increases = 0;
-    for i in 3..depths.len() {
-        if depths[i - 2] + depths[i - 1] + depths[i] > depths[i - 3] + depths[i - 2] + depths[i - 1] {
-            num_increases += 1;
-        }
-    }
-    num_increases
+pub fn solve_part2(depths: &[u32]) -> usize {
+    depths
+        .windows(3)
+        .tuple_windows()
+        .filter(|(first_window, second_window)| {
+            second_window.iter().sum::<u32>() > first_window.iter().sum()
+        })
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1() {
-        static INPUT: &str = "199
+    static INPUT: &str = "199
 200
 208
 210
@@ -39,23 +37,14 @@ mod tests {
 260
 263";
 
+    #[test]
+    fn test_part1() {
         let depths = input_generator(INPUT);
         assert_eq!(7, solve_part1(&depths));
     }
 
     #[test]
     fn test_part2() {
-        static INPUT: &str = "199
-200
-208
-210
-200
-207
-240
-269
-260
-263";
-
         let depths = input_generator(INPUT);
         assert_eq!(5, solve_part2(&depths));
     }
